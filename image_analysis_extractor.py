529f6c0627ba0f16d5a451b4c97243cbeee0e8c6
bingzhang <bing@illinois.edu>
2025-04-21 11:33:28 -0500

    average runs

--- image_analysis_extractor.py
+++ image_analysis_extractor.py
@@ -43,8 +43,14 @@ class ImageAnalysisExtractor(Extractor):
             metadata = resource['metadata']
             campaign_id = metadata['campaign_id']
             cell_id = metadata['cell_id']
+            rank_run = metadata['rank_run']
+            number_prints_trigger_prediction = metadata['number_prints_trigger_prediction']
+            accum_h_mu = float(metadata['accum_h_mu'])
+
             print("campaign_id", campaign_id)
             print("cell_id", cell_id)
+            print('rank_run')
+            print('number_prints_trigger_prediction')
             inputfile = pyclowder.files.download(connector, host, secret_key, resource['id'])
             H_DIST, h_mu, h_sig, V_DIST, v_mu, v_sig, S_DIST, s_mu, s_sig = image_analysis(inputfile)
             content = {
@@ -62,19 +68,31 @@ class ImageAnalysisExtractor(Extractor):
 
             # upload metadata
             # pyclowder.files.upload_metadata(connector, host, secret_key, parameters['id'], metadata)
-
-            if self.campaign_id is None or self.campaign_id != campaign_id:
+            data = None
+            if rank_run % number_prints_trigger_prediction == 0:
                 self.campaign_id = campaign_id
                 self.opt = optimizer_init()
+            if rank_run +1 == number_prints_trigger_prediction:
+                accum_h_mu += h_mu
+                h_mu = accum_h_mu/number_prints_trigger_prediction
+                PrintSpeed, BedTemp, Pressure, ZHeight = optimizer_get(self.opt)
+                _ = optimizer_tell(self.opt, h_mu, PrintSpeed, BedTemp, Pressure, ZHeight)
+                data = {"campaign_id": campaign_id, "cell_id": cell_id, "file_id": file_id, 'rank_run': rank_run,
+                        "cell_color": content,
+                        "PrintSpeed": PrintSpeed, "BedTemp": BedTemp, "Pressure": Pressure, "ZHeight": ZHeight}
+            else:
+                data = {"campaign_id": campaign_id, "cell_id": cell_id, "file_id": file_id, "cell_color": content,
+                         'rank_run': rank_run}
+            # if self.campaign_id is None or self.campaign_id != campaign_id:
+            #     self.campaign_id = campaign_id
+            #     self.opt = optimizer_init()
 
-            PrintSpeed, BedTemp, Pressure, ZHeight = optimizer_get(self.opt)
-            _ = optimizer_tell(self.opt, h_mu, PrintSpeed, BedTemp, Pressure, ZHeight)
+            # PrintSpeed, BedTemp, Pressure, ZHeight = optimizer_get(self.opt)
+            # _ = optimizer_tell(self.opt, h_mu, PrintSpeed, BedTemp, Pressure, ZHeight)
             # store backt tp SCP web application
             try:
                 url = SCP_WEB_URL_BASE + 'campaign/%s/update_cell_color' % (campaign_id)
-                result = requests.post(url, data=json.dumps(
-                    {"campaign_id": campaign_id, "cell_id": cell_id, "file_id": file_id, "cell_color": content,
-                     "PrintSpeed": PrintSpeed, "BedTemp": BedTemp, "Pressure": Pressure, "ZHeight": ZHeight}),
+                result = requests.post(url, data=json.dumps(data),
                                        headers={'Content-type': 'application/json', 'accept': 'application/json'},
                                        verify=False)
                 result.raise_for_status()
